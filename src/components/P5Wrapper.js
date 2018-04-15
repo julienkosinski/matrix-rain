be4d9276e86ee0bc825f13655360ac781f02880a
Julien Kosinski <julien.kosinski@gmail.com>
2018-04-16 00:59:32 +0200

    Fix a memory leak
    See the original Pull Request:
    https://github.com/NeroCor/react-p5-wrapper/pull/15

--- src/components/P5Wrapper.js
+++ src/components/P5Wrapper.js
@@ -2,37 +2,44 @@
 https://github.com/NeroCor/react-p5-wrapper
 */
 import React from 'react';
+let p5;
 if (process.env.NODE_ENV === 'production') {
-  var p5 = require('p5/lib/p5.min');
+  p5 = require('p5/lib/p5.min');
 } else {
   /* using unminified slows down animation a lot
      so use p5.min.js if you want the animation to be fast.
      Unminified may still be helpful for debugging so leaving it
      on as default in dev build */
-  var p5 = require('p5');
+  p5 = require('p5');
 }
 
 class P5Wrapper extends React.Component {
 
   componentDidMount() {
     this.canvas = new p5(this.props.sketch, this.wrapper);
-    if( this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
+    if (this.canvas.myCustomRedrawAccordingToNewPropsHandler) {
       this.canvas.myCustomRedrawAccordingToNewPropsHandler(this.props);
     }
   }
 
   componentWillReceiveProps(newprops) {
-    if(this.props.sketch !== newprops.sketch){
-      this.wrapper.removeChild(this.wrapper.childNodes[0]);
+    if (this.props.sketch !== newprops.sketch) {
+      this.canvas.remove();
       this.canvas = new p5(newprops.sketch, this.wrapper);
     }
-    if(this.canvas.myCustomRedrawAccordingToNewPropsHandler ) {
+    if (this.canvas.myCustomRedrawAccordingToNewPropsHandler) {
       this.canvas.myCustomRedrawAccordingToNewPropsHandler(newprops);
     }
   }
 
+  componentWillUnmount() {
+    this.canvas.remove();
+  }
+
   render() {
-    return <div ref={wrapper => this.wrapper = wrapper}></div>;
+    return <div ref = {
+      wrapper => this.wrapper = wrapper
+    } > < /div>;
   }
 }
 
